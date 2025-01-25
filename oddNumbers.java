ad903d1326678ee4a289d24e2f4daa314620b681
Ramya Harshini V S <ramyaaharshinivs8357@gmail.com>
2025-01-25 14:59:08 +0530

    Add files via upload

---
+++ oddNumbers.java
@@ -0,0 +1,14 @@
+class oddNumbers{
+ public static void main(String args[]){
+  int i,n=100;
+  System.out.println("Displaying odd numbers from 1 to 100");
+  for (i=0;i<=n;i++){
+    if(i%2==1){
+     System.out.println( i + " ");
+}
+}
+}
+}
+
+
+
