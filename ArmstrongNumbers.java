7299b837645fb5489d5525aabfce4c55247edacb
ramyaavs21 <ramyaharshinivs11a@gmail.com>
2025-01-25 15:17:29 +0530

    Update ReverseNumberjava.java
    Changes done

--- ArmstrongNumbers.java
+++ ArmstrongNumbers.java
@@ -1,26 +1,20 @@
-public class ArmstrongNumbers  
-{ 
- 
- public static void main(String[] args)  
- { 
-  int num,rem,limit=1000, sum = 0; 
-  System.out.print("Armstrong numbers from 1 to N:"); 
-  for (int i = 1; i <= limit; i++) 
-               
-  { 
-   num = i; 
-   while (num > 0) 
-   { 
-    rem = num % 10; 
-    sum = sum + (rem*rem*rem); 
-    num = num / 10; 
-   } 
-   
-   if (sum == i) 
-   { 
-    System.out.print(i + " "); 
-   } 
-   sum = 0; 
-  } 
- } 
-} 
+public class ReverseNumberjava
+{
+    	public static void main(String[] args) 
+	{
+        
+        	int number = 12345; 
+        	int reversedNumber = 0;
+
+        
+        	for (int temp = number; temp > 0; temp /= 10) 
+		{
+            		int digit = temp % 10; 
+            		reversedNumber = reversedNumber * 10 + digit; 
+        	}
+
+        
+        	System.out.println("Original number: " + number);
+        	System.out.println("Reversed number: " + reversedNumber);
+    	}
+}
